917d924cb6466ade01cb9b2c342584478eb8ac7a
DanielKuhnDeAraujo <danielaprendendo00@gmail.com>
2026-01-23 23:00:23 -0300

    23:00

--- secenes/quadrado.gd
+++ secenes/quadrado.gd
@@ -41,3 +41,6 @@ func checar_idade() :
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+@warning_ignore("unused_parameter")
+func interagir(cena ,idade, nome ,sprite) :
+	print("interação" )
