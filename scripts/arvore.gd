6e22b31dbac32fa3e023ac452f3f7f493b3e59ed
me-cajoca <cibcosta177@gmail.com>
2026-01-25 18:39:40 -0300

    arvora fazida with sucess (ou 99,99% caso tenha erros)
    arvore fazida

--- scripts/arvore.gd
+++ scripts/arvore.gd
@@ -2,11 +2,16 @@ extends RigidBody2D
 
 @onready var sprite_2d: Sprite2D = $Semente
 
+#isso é so p ajustar a posicao
 @onready var muda_pos: Marker2D = $Muda_pos
 @onready var tree_pos: Marker2D = $Tree_pos
+@onready var sem_pos: Marker2D = $Sem_pos
+
+@onready var col_broto: CollisionShape2D = $Col_broto
+@onready var col_semente: CollisionShape2D = $Col_semente
 
 var Idade = -10 
-var Sprite = "res://assets/Semente.png"
+var Sprite
 var Cena = "res://secenes/arvoree.tscn"
 var Nome = "Arvore"
 var pegavel = true
@@ -14,12 +19,14 @@ var pegavel = true
 var Spassado 
 var Spresente 
 var Sfuturo
+var Smorta
 
 # Called when the node enters the scene tree for the first time.
 func preload_resources() :
-	Spassado= preload("res://assets/Muda.png")
-	Spresente= preload("res://assets/Semente.png")
+	Spassado= preload("res://assets/Semente.png")
+	Spresente= preload("res://assets/Muda.png")
 	Sfuturo= preload("res://assets/Arvore.png")
+	Smorta = preload("res://assets/Árvore morta.png")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	preload_resources()
@@ -35,26 +42,49 @@ func passado() :
 func checar_idade() :
 	Nome= "Pau de "+ str(Idade)+ " anos."
 	if Idade < 0 : #deixar vazio
+		pegavel = true
+		sprite_2d.position = muda_pos.position
 		sprite_2d.texture = Spassado
 		Sprite = Spassado
+		col_semente.disabled = false
+	else:
+		rotation = 0
+
+		
+		
 		
 	if Idade == 0 :
-		
+		sprite_2d.scale = Vector2(1,1)
+		rotation = 0
+		pegavel = true
 		sprite_2d.texture = Spresente
-		sprite_2d.global_position = muda_pos.global_position
+		sprite_2d.position = muda_pos.position
 		Sprite = Spresente
 		
-	if Idade > 0 :
+		
+		
+	if Idade > 0 and Idade < 40:
+		set_collision_layer_value(3, true)
+		sprite_2d.scale = Vector2(0.186, 0.186)
+		
 		pegavel = false
 		sprite_2d.texture = Sfuturo
 		Sprite = Sfuturo
-		sprite_2d.global_position = tree_pos.global_position
-		sprite_2d.scale = Vector2(0.192, 0.192)
+		sprite_2d.position = tree_pos.position
+
 		$Col_Folhas.disabled = false
 		
 	else:
-		pegavel = true
+		set_collision_layer_value(3, false)
 		$Col_Folhas.disabled = true
+		
+	if Idade >= 40:
+		sprite_2d.position = tree_pos.position
+		pegavel = false
+		sprite_2d.texture = Smorta
+		sprite_2d.position = tree_pos.position
+		Sprite = Smorta
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 	
