6e2446a645a6f2cb5592514188e537db8d14d628
DanielKuhnDeAraujo <danielaprendendo00@gmail.com>
2026-01-26 11:53:17 -0300

    mais coisa

--- scripts/tutoras.gd
+++ scripts/tutoras.gd
@@ -3,11 +3,16 @@ extends Node2D
 var filhos
 @onready var placeholderback: Sprite2D = $Placeholderback
 @onready var back_2_place: Sprite2D = $Back2Place
+@onready var rei: AnimatableBody2D = $rei
+@onready var tempo: Label = $tempo
+@onready var cena_1: Marker2D = $cena1
 var viajantes
 var viajando:bool = false 
+var viagem = false
+var cena = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	cutscene1()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,11 +21,18 @@ func _process(delta: float) -> void:
 		if viajantes == 0 :
 			get_node("Player").viajando = false
 			viajando = false
-	if placeholderback.modulate.a > 0 :
-		placeholderback.modulate.a -= 0.2*delta
-	if back_2_place.modulate.a < 1 :
-		back_2_place.modulate.a += 0.2*delta
-	
+			
+	if viagem :
+		if placeholderback.modulate.a > 0 :
+			placeholderback.modulate.a -= 0.3*delta
+		if back_2_place.modulate.a < 1 :
+			back_2_place.modulate.a += 0.3*delta
+		else :
+			viagem =false
+	match cena :
+		1:
+			rei.position = rei.position.move_toward(cena_1.position, 100 * delta)
+		
 func futuro():
 	viajando = true 
 	viajantes =0
@@ -42,4 +54,7 @@ func passado():
 func finale(cena ,idade, nome ,sprite) :
 	pass
 func cutscene1() :
-	pass
+	viagem =true
+	tempo.queue_free()
+	cena = 1
+	
